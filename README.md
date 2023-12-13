7c9e4df120e1f6541a4174a2f83af229e88a2a68
Francesco Grazioso <francescograzioso1995@gmail.com>
2023-12-13 13:14:45 +0100

    add otions and wrote readme

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # TikTokVideoMaker
-A python script to automatically segment and format a video for TikTok
+
+This script allows you to split a video into segments, optionally adding gameplay footage to each segment.
+
+## Prerequisites
+
+- Python 3
+- Required Python libraries are listed in `requirements.txt`. Install them using:
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+## Usage
+
+```bash
+python script.py -v <input_video_path> [-l <segment_length>] [-g]
+```
+
+    -v, --video: Path to the input video file (required).
+    -l, --length: Length of each video segment in seconds (default: 60, max: 120).
+    -g, --gameplay: Add gameplay video to each segment (default: True).
+
+## Example
+
+```bash
+
+python script.py -v input_video.mp4 -l 45 -g
+```
+
+This example splits the video "input_video.mp4" into segments of 45 seconds each, adding gameplay footage to each segment.
+Notes
+
+    Ensure that ffmpeg is installed on your system, and the path is correctly set (IMAGEIO_FFMPEG_EXE).
+    The script will create an 'output' folder to store the segmented videos.
+
+Feel free to modify the options according to your needs. For additional help, use:
+
+```bash
+python script.py -h
+```
+
+Note: Adjust the paths and filenames in the script according to your file structure.
+
+This `README.md` provides a brief overview of the script, its usage, and import
--- converter.py
+++ converter.py
@@ -3,26 +3,27 @@ os.environ["IMAGEIO_FFMPEG_EXE"] = "/opt/homebrew/bin/ffmpeg"
 import sys
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
-import cv2
+import argparse
 
-def split_video(input_path, output_folder):
-    # Crea la cartella di output se non esiste
+def split_video(input_path, output_folder, segment_length, add_gameplay):
+    # Create the output folder if it doesn't exist
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    # Ottieni il nome del file senza estensione
+    # Get the base name of the file without extension
     base_name = os.path.splitext(os.path.basename(input_path))[0]
 
-    # Carica il video principale
+    # Load the main video clip
     video_clip = VideoFileClip(input_path)
 
-    # Durata massima per ciascun segmento (in secondi)
-    segment_duration = 60
+    # Maximum duration for each segment (in seconds)
+    max_segment_duration = 120
+    segment_duration = min(segment_length, max_segment_duration) if segment_length else 60
 
-    # Calcola il numero di segmenti
+    # Calculate the number of segments
     num_segments = int(video_clip.duration // segment_duration) + 1
 
-    # Divide il video in segmenti
+    # Divide the video into segments
     video_segments = []
     for i in range(num_segments):
         start_time = i * segment_duration
@@ -30,34 +31,39 @@ def split_video(input_path, output_folder):
         segment = video_clip.subclip(start_time, end_time)
         video_segments.append(segment)
 
-    # Carica il video di gameplay
-    gameplay_clip = VideoFileClip("gameplay.mp4")
-    gameplay_clip.audio = None
+    # Load the gameplay video if requested
+    if add_gameplay:
+        gameplay_clip = VideoFileClip("gameplay.mp4")
+        gameplay_clip.audio = None
+    else:
+        gameplay_clip = None
 
-    # Creazione della cartella di output
+    # Create the output folder
     output_path = os.path.join(output_folder, base_name)
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
-    # Composizione dei video segmentati con il gameplay e salvataggio
+    # Compose the segmented videos with gameplay (if present) and save
     for i, segment in enumerate(video_segments):
-        adjusted_gameplay_clip = gameplay_clip.subclip(0, segment.duration)
-        output_clip = CompositeVideoClip([segment.set_position(('center', 'top')),
-                                          adjusted_gameplay_clip.set_position(('center', 'bottom'))],
-                                         size=(segment.size[0], segment.size[1] + adjusted_gameplay_clip.size[1]))
+        if add_gameplay:
+            adjusted_gameplay_clip = gameplay_clip.subclip(0, segment.duration)
+            output_clip = CompositeVideoClip([segment.set_position(('center', 'top')),
+                                              adjusted_gameplay_clip.set_position(('center', 'bottom'))],
+                                             size=(segment.size[0], segment.size[1] + adjusted_gameplay_clip.size[1]))
+        else:
+            output_clip = segment
 
         output_clip.write_videofile(os.path.join(output_path, f"{base_name}_segment_{i + 1}.mp4"), codec="libx264", audio_codec="aac")
 
-    print("Elaborazione completata. I video sono stati salvati nella cartella 'output'.")
+    print("Processing complete. Videos have been saved in the 'output' folder.")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Utilizzo: python script.py path/del/video")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description="Split and compose video segments with gameplay.")
+    parser.add_argument("-v", "--video", help="Path to the input video file.", required=True)
+    parser.add_argument("-l", "--length", type=int, help="Length of each video segment in seconds (default: 60, max: 120).")
+    parser.add_argument("-g", "--gameplay", action="store_true", help="Add gameplay video to each segment (default: True).")
+    args = parser.parse_args()
 
-    input_path = sys.argv[1]
+    input_path = args.video
     output_folder = "output"
-    split_video(input_path, output_folder)
-
-
-
+    split_video(input_path, output_folder, args.length, args.gameplay)
